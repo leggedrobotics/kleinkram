17e8a9f25e22666db92cb5f8c570529e0616ac12
Johann Schwabe <johann@schwabe.dev>
2024-05-27 14:36:07 +0200

    added actions

--- CLI/src/ETH_GTD_cli/main.py
+++ CLI/src/ETH_GTD_cli/main.py
@@ -28,27 +28,33 @@ app.add_typer(queue)
 app.add_typer(user)
 app.command()(login)
 
-@files.command('list')
-def list_files(project: Annotated[str, typer.Option()] = None,
-               run: Annotated[str, typer.Option()] = None,
-               topics: Annotated[List[str], typer.Option()] = None):
+
+@files.command("list")
+def list_files(
+    project: Annotated[str, typer.Option()] = None,
+    run: Annotated[str, typer.Option()] = None,
+    topics: Annotated[List[str], typer.Option()] = None,
+):
     """
     List all files with optional filters for project, run, or topics.
     """
     try:
         url = f"{API_URL}/file/filteredByNames"
-        response = client.get(url, params={
-            'projectName': project,
-            'runName': run,
-            'topics': topics,
-        })
+        response = client.get(
+            url,
+            params={
+                "projectName": project,
+                "runName": run,
+                "topics": topics,
+            },
+        )
         response.raise_for_status()
         data = response.json()
         runs_by_project_uuid = {}
         files_by_run_uuid = {}
         for file in data:
-            run_uuid = file['run']['uuid']
-            project_uuid = file['run']['project']['uuid']
+            run_uuid = file["run"]["uuid"]
+            project_uuid = file["run"]["project"]["uuid"]
             if project_uuid not in runs_by_project_uuid:
                 runs_by_project_uuid[project_uuid] = []
             if run_uuid not in runs_by_project_uuid[project_uuid]:
@@ -57,7 +63,7 @@ def list_files(project: Annotated[str, typer.Option()] = None,
                 files_by_run_uuid[run_uuid] = []
             files_by_run_uuid[run_uuid].append(file)
 
-        print('Files by Run & Project:')
+        print("Files by Run & Project:")
         for project_uuid, runs in runs_by_project_uuid.items():
             first_file = files_by_run_uuid[runs[0]][0]
             print(f"* {first_file['run']['project']['name']}")
@@ -69,7 +75,8 @@ def list_files(project: Annotated[str, typer.Option()] = None,
     except client.HTTPError as e:
         print(f"Failed to fetch runs: {e}")
 
-@projects.command('list')
+
+@projects.command("list")
 def list_projects():
     """
     List all projects.
@@ -78,7 +85,7 @@ def list_projects():
         response = client.get(f"{API_URL}/project")
         response.raise_for_status()
         projects = response.json()
-        print('Projects:')
+        print("Projects:")
         for project in projects:
             print(f"- {project['name']}")
 
@@ -86,8 +93,10 @@ def list_projects():
         print(f"Failed to fetch projects: {e}")
 
 
-@runs.command('list')
-def list_runs(project: Annotated[str, typer.Option()]=None,):
+@runs.command("list")
+def list_runs(
+    project: Annotated[str, typer.Option()] = None,
+):
     """
     List all runs with optional filter for project.
     """
@@ -102,12 +111,12 @@ def list_runs(project: Annotated[str, typer.Option()]=None,):
         data = response.json()
         runs_by_project_uuid = {}
         for run in data:
-            project_uuid = run['project']['uuid']
+            project_uuid = run["project"]["uuid"]
             if project_uuid not in runs_by_project_uuid:
                 runs_by_project_uuid[project_uuid] = []
             runs_by_project_uuid[project_uuid].append(run)
 
-        print('Runs by Project:')
+        print("Runs by Project:")
         for project_uuid, runs in runs_by_project_uuid.items():
             print(f"* {runs_by_project_uuid[project_uuid][0]['project']['name']}")
             for run in runs:
@@ -116,8 +125,12 @@ def list_runs(project: Annotated[str, typer.Option()]=None,):
     except client.HTTPError as e:
         print(f"Failed to fetch runs: {e}")
 
+
 @topics.command("list")
-def topics(file: Annotated[str, typer.Option()] = None, full: Annotated[bool, typer.Option()] = False):
+def topics(
+    file: Annotated[str, typer.Option()] = None,
+    full: Annotated[bool, typer.Option()] = False,
+):
     try:
         url = API_URL + "/file/byName"
         response = client.get(url, params={"name": file})
@@ -129,11 +142,18 @@ def topics(file: Annotated[str, typer.Option()] = None, full: Annotated[bool, ty
         else:
             table = Table("UUID", "name", "type", "nrMessages", "frequency")
             for topic in data["topics"]:
-                table.add_row(topic["uuid"], topic["name"], topic["type"], topic["nrMessages"], f"{topic['frequency']}")
+                table.add_row(
+                    topic["uuid"],
+                    topic["name"],
+                    topic["type"],
+                    topic["nrMessages"],
+                    f"{topic['frequency']}",
+                )
             print(table)
 
     except client.HTTPError as e:
         print(f"Failed")
+
 
 @projects.command("create")
 def create_project(name: Annotated[str, typer.Option()]):
@@ -146,10 +166,13 @@ def create_project(name: Annotated[str, typer.Option()]):
     except client.HTTPError as e:
         print(f"Failed to create project: {e}")
 
+
 @app.command("upload")
-def upload(path: Annotated[str, typer.Option(prompt=True)],
-           project: Annotated[str, typer.Option(prompt=True)],
-           run: Annotated[str, typer.Option(prompt=True)]):
+def upload(
+    path: Annotated[str, typer.Option(prompt=True)],
+    project: Annotated[str, typer.Option(prompt=True)],
+    run: Annotated[str, typer.Option(prompt=True)],
+):
     files = expand_and_match(path)
     filenames = list(map(lambda x: x.split("/")[-1], files))
     filepaths = {}
@@ -173,20 +196,26 @@ def upload(path: Annotated[str, typer.Option(prompt=True)],
         if run_response.content:
             run_json = run_response.json()
             if run_json["uuid"]:
-                print(f"Run: {run_json['uuid']} already exists. Delete it or select another name.")
+                print(
+                    f"Run: {run_json['uuid']} already exists. Delete it or select another name."
+                )
                 return
             print(f"Something failed, should not happen")
             return
 
         create_run_url = API_URL + "/run/create"
-        new_run = client.post(create_run_url, json={"name": run, "projectUUID": project_json["uuid"]})
+        new_run = client.post(
+            create_run_url, json={"name": run, "projectUUID": project_json["uuid"]}
+        )
         new_run.raise_for_status()
         new_run_data = new_run.json()
         print(f"Created run: {new_run_data['name']}")
 
-
         get_presigned_url = API_URL + "/queue/createPreSignedURLS"
-        response_2 = client.post(get_presigned_url, json={"filenames": filenames, "runUUID": new_run_data["uuid"]})
+        response_2 = client.post(
+            get_presigned_url,
+            json={"filenames": filenames, "runUUID": new_run_data["uuid"]},
+        )
         response_2.raise_for_status()
         presigned_urls = response_2.json()
         for file in filenames:
@@ -195,43 +224,46 @@ def upload(path: Annotated[str, typer.Option(prompt=True)],
         if len(presigned_urls) > 0:
             uploadFiles(presigned_urls, filepaths, 4)
 
-
-
     except client.HTTPError as e:
         print(e)
 
-@queue.command('clear')
+
+@queue.command("clear")
 def clear_queue():
     """Clear queue"""
     # Prompt the user for confirmation
     confirmation = typer.prompt("Are you sure you want to clear the queue? (y/n)")
-    if confirmation.lower() == 'y':
+    if confirmation.lower() == "y":
         response = client.delete(f"{API_URL}/queue/clear")
         response.raise_for_status()
         print("Queue cleared.")
     else:
         print("Operation cancelled.")
 
-@files.command('clear')
+
+@files.command("clear")
 def clear_queue():
     """Clear queue"""
     # Prompt the user for confirmation
     confirmation = typer.prompt("Are you sure you want to clear the Files? (y/n)")
-    if confirmation.lower() == 'y':
+    if confirmation.lower() == "y":
         response = client.delete(f"{API_URL}/file/clear")
         response.raise_for_status()
         print("Files cleared.")
     else:
         print("Operation cancelled.")
 
-@app.command('wipe')
+
+@app.command("wipe")
 def wipe():
     """Wipe all data"""
     # Prompt the user for confirmation
     confirmation = typer.prompt("Are you sure you want to wipe all data? (y/n)")
-    if confirmation.lower() == 'y':
-        second_confirmation = typer.prompt("This action is irreversible. Are you really sure? (y/n)")
-        if second_confirmation.lower() != 'y':
+    if confirmation.lower() == "y":
+        second_confirmation = typer.prompt(
+            "This action is irreversible. Are you really sure? (y/n)"
+        )
+        if second_confirmation.lower() != "y":
             print("Operation cancelled.")
             return
 
@@ -257,14 +289,15 @@ def wipe():
     else:
         print("Operation cancelled.")
 
-@app.command('claim')
+
+@app.command("claim")
 def claim():
     response = client.post(f"{API_URL}/user/claimAdmin")
     response.raise_for_status()
     print("Admin claimed.")
 
 
-@user.command('list')
+@user.command("list")
 def users():
     response = client.get(f"{API_URL}/user/all")
     response.raise_for_status()
@@ -274,24 +307,28 @@ def users():
         table.add_row(user["name"], user["email"], user["role"], user["googleId"])
     print(table)
 
-@user.command('info')
+
+@user.command("info")
 def user_info():
     response = client.get(f"{API_URL}/user/me")
     response.raise_for_status()
     data = response.json()
     print(data)
 
-@user.command('promote')
+
+@user.command("promote")
 def promote(email: Annotated[str, typer.Option()]):
     response = client.post(f"{API_URL}/user/promote", json={"email": email})
     response.raise_for_status()
     print("User promoted.")
 
-@user.command('demote')
+
+@user.command("demote")
 def demote(email: Annotated[str, typer.Option()]):
     response = client.post(f"{API_URL}/user/demote", json={"email": email})
     response.raise_for_status()
     print("User demoted.")
+
 
 if __name__ == "__main__":
     app()
