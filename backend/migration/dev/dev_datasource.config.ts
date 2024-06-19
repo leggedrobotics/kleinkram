60f67678060b02bfa8ab4f59b1301ecaa2fc47dc
Cyrill Püntener <wp99cp@gmx.net>
2024-06-19 10:34:56 +0200

    fix env retrieval for dev migrations

--- backend/migration/dev/dev_datasource.config.ts
+++ backend/migration/dev/dev_datasource.config.ts
@@ -7,12 +7,12 @@ dotenv.config({ path: './migration/.env' });
 export function getConfig() {
     return {
         type: 'postgres',
-        host: process.env.local_dbhost,
-        port: parseInt(process.env.local_dbport, 10),
-        ssl: process.env.local_ssl === 'true',
-        username: process.env.local_dbuser,
-        password: process.env.local_dbpassword,
-        database: process.env.local_dbname,
+        host: process.env.dev_dbhost,
+        port: parseInt(process.env.dev_port, 10),
+        ssl: process.env.dev_ssl === 'true',
+        username: process.env.dev_dbuser,
+        password: process.env.dev_dbpassword,
+        database: process.env.dev_dbname,
         synchronize: false,
         migrations: ['migration/dev/migrations/*.ts'],
         entities: ['src/**/*.entity{.ts,.js}'],
