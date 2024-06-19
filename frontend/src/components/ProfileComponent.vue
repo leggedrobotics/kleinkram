fe3bb61dcaf9d126478448821b2d13d85aa1ea85
Cyrill Püntener <wp99cp@gmx.net>
2024-06-19 08:51:01 +0200

    update user profile

--- frontend/src/components/ProfileComponent.vue
+++ frontend/src/components/ProfileComponent.vue
@@ -4,7 +4,6 @@ import ROUTES from "src/router/routes";
 
 const is_authenticated = await isAuthenticated();
 const user = await getUser();
-
 </script>
 
 <template>
@@ -14,9 +13,14 @@ const user = await getUser();
 
     <div class="q-gutter-sm row items-center no-wrap" v-else>
       <q-btn round flat>
-        <q-avatar size="26px">
+        <q-avatar size="32px" v-if="!!user['avatarUrl'] && user['avatarUrl'] !== ''">
           <img :src="user['avatarUrl']" referrerpolicy="no-referrer" alt="avatar">
         </q-avatar>
+
+        <q-avatar size="36px" v-else>
+          <q-icon name="account_circle"/>
+        </q-avatar>
+
         <q-menu>
           <q-list dense style="min-width: 150px; text-align: right">
             <q-item clickable v-close-popup>
