72eacdf33a827223c94af27f3f24fcc8363928ac
Cyrill Püntener <wp99cp@gmx.net>
2024-06-19 09:42:48 +0200

    make top nav fully responsive

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,31 +1,53 @@
 <template>
-  <q-header bordered class="bg-white text-grey-9">
-    <q-toolbar>
+    <q-header bordered class="bg-white text-grey-9">
+        <q-toolbar>
 
-      <q-btn flat round dense icon="menu"/>
+            <q-btn flat dense round icon="menu" v-show="$q.screen.lt.md">
 
-      <q-toolbar-title shrink>
-        Bagistry
-      </q-toolbar-title>
+                <q-menu auto-close style="width: 280px">
+                    <q-list>
+                        <q-item v-for="item in main_menu" :key="item.title" clickable :to="item.to">
+                            <q-item-section avatar>
+                                <q-icon :name="item.icon" />
+                            </q-item-section>
+                            <q-item-section>
+                                <q-item-label>{{ item.title }}</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-menu>
 
-      <q-tabs>
-        <q-route-tab :to="ROUTES.HOME.path"> Home</q-route-tab>
-        <q-route-tab :to="ROUTES.DATATABLE.path"> Datatable</q-route-tab>
-        <q-route-tab :to="ROUTES.EXPLORER.path"> Explorer</q-route-tab>
-        <q-route-tab :to="ROUTES.UPLOAD.path"> Upload</q-route-tab>
-        <q-route-tab :to="ROUTES.ACTION.path"> Analysis</q-route-tab>
-      </q-tabs>
+            </q-btn>
 
-      <q-space/>
-      <Suspense>
-        <ProfileComponent/>
-      </Suspense>
 
-    </q-toolbar>
-  </q-header>
+            <q-toolbar-title shrink style="margin-right: 30px">
+                Bagistry
+            </q-toolbar-title>
+
+            <q-tabs v-show="$q.screen.gt.sm" inline-label>
+                <q-route-tab v-for="item in main_menu" :key="item.title" :label="item.title" :to="item.to"
+                       :icon="item.icon" />
+            </q-tabs>
+
+            <q-space />
+            <Suspense>
+                <ProfileComponent />
+            </Suspense>
+
+        </q-toolbar>
+    </q-header>
 </template>
 
 <script setup lang="ts">
 import ROUTES from 'src/router/routes';
-import ProfileComponent from "components/ProfileComponent.vue";
+import ProfileComponent from 'components/ProfileComponent.vue';
+
+const main_menu = [
+    { title: 'Home', icon: 'home', to: ROUTES.HOME.path },
+    { title: 'Datatable', icon: 'table_chart', to: ROUTES.DATATABLE.path },
+    { title: 'Explorer', icon: 'explore', to: ROUTES.EXPLORER.path },
+    { title: 'Upload', icon: 'cloud_upload', to: ROUTES.UPLOAD.path },
+    { title: 'Analysis', icon: 'analytics', to: ROUTES.ACTION.path },
+];
+
 </script>
--- frontend/src/components/ProfileComponent.vue
+++ frontend/src/components/ProfileComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {getUser, isAuthenticated, logout} from 'src/services/auth';
-import ROUTES from "src/router/routes";
+import { getUser, isAuthenticated, logout } from 'src/services/auth';
+import ROUTES from 'src/router/routes';
 
 const is_authenticated = await isAuthenticated();
 const user = await getUser();
@@ -8,37 +8,42 @@ const user = await getUser();
 
 <template>
 
-  <q-tabs>
-    <q-route-tab :to="ROUTES.LOGIN.path" v-if="!is_authenticated"> Sign in</q-route-tab>
+    <q-tabs>
+        <q-route-tab :to="ROUTES.LOGIN.path" v-if="!is_authenticated"> Sign in</q-route-tab>
 
-    <div class="q-gutter-sm row items-center no-wrap" v-else>
-      <q-btn round flat>
-        <q-avatar size="32px" v-if="!!user['avatarUrl'] && user['avatarUrl'] !== ''">
-          <img :src="user['avatarUrl']" referrerpolicy="no-referrer" alt="avatar">
-        </q-avatar>
+        <div class="q-gutter-sm row items-center no-wrap" v-else>
+            <q-btn round flat>
+                <q-avatar size="32px" v-if="!!user['avatarUrl'] && user['avatarUrl'] !== ''">
+                    <img :src="user['avatarUrl']" referrerpolicy="no-referrer" alt="avatar">
+                </q-avatar>
 
-        <q-avatar size="36px" v-else>
-          <q-icon name="account_circle"/>
-        </q-avatar>
+                <q-avatar size="36px" v-else>
+                    <q-icon name="account_circle" />
+                </q-avatar>
 
-        <q-menu>
-          <q-list dense style="min-width: 150px; text-align: right">
-            <q-item clickable v-close-popup>
-              <q-item-section>{{ user['name'] }}</q-item-section>
-            </q-item>
-            <q-separator/>
-            <q-item clickable>
-              <q-item-section>
-                <q-icon name="logout"/>
-              </q-item-section>
-              <q-item-section @click="logout">Sign out</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-    </div>
+                <q-menu auto-close style="width: 280px">
+                    <q-list>
+                        <q-item clickable v-close-popup>
+                            <q-item-section avatar></q-item-section>
+                            <q-item-section>
+                                <q-item-section>{{ user['name'] }}</q-item-section>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable @click="logout">
+                            <q-item-section avatar>
+                                <q-icon name="logout" />
+                            </q-item-section>
+                            <q-item-section>
+                                <q-item-label>Sign out</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-menu>
 
-  </q-tabs>
+            </q-btn>
+        </div>
+
+    </q-tabs>
 
 </template>
 
