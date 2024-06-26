8cfc75920332fddbb2eba0d41a3a92045b41ba7a
Cyrill Püntener <wp99cp@gmx.net>
2024-06-26 18:01:46 +0200

    allow for multiple actions to run in parallel

--- queueConsumer/src/actions/container_scheduler.ts
+++ queueConsumer/src/actions/container_scheduler.ts
@@ -1,8 +1,10 @@
 import Docker from 'dockerode';
 import Dockerode from 'dockerode';
-import { tracing } from '../tracing';
-import logger, { ACTION_CONTAINER_LABEL } from '../logger';
-import { ContainerLog } from '@common/entities/action/action.entity';
+import {tracing} from '../tracing';
+import logger from '../logger';
+import Action, {ContainerLog} from '@common/entities/action/action.entity';
+import {ActionState} from "@common/enum";
+import {Repository} from "typeorm";
 
 export type ContainerLimits = {
     /**
@@ -41,13 +43,21 @@ export type ContainerOptions = {
     environment?: ContainerEnv;
 }
 
+export const dockerDaemonErrorHandling = (error: Error) => {
+    logger.error(error.message)
+    logger.error(error.stack)
+    return null;
+}
 
 export class ContainerScheduler {
 
     protected readonly docker: Docker;
+    protected static readonly CONTAINER_PREFIX = 'datasets-runner-';
+    protected actionRepository: Repository<Action>;
 
-    constructor() {
-        this.docker = new Docker({ socketPath: '/var/run/docker.sock' });
+    constructor(actionRepository: Repository<Action>) {
+        this.actionRepository = actionRepository;
+        this.docker = new Docker({socketPath: '/var/run/docker.sock'});
     }
 
     /**
@@ -70,19 +80,17 @@ export class ContainerScheduler {
         // merge the given container limitations with the default ones
         if (!container_options) container_options = {};
         container_options = {
-            ...defaultContainerLimitations,
             ...container_options,
-            limits: { ...defaultContainerLimitations, ...container_options?.limits },
-            environment: { ...container_options?.environment },
+            limits: {...defaultContainerLimitations, ...container_options?.limits},
+            environment: {...container_options?.environment},
         };
 
         logger.debug(`Starting container with options: ${JSON.stringify(container_options)}`);
 
         // assert that we only run rslethz images
-        // TODO: uncomment this line when we have our own images
-        // if (!docker_image.startsWith('rslethz/')) {
-        //    throw new Error('Only images from the rslethz organization are allowed');
-        // }
+        if (!container_options.docker_image.startsWith('rslethz/')) {
+            throw new Error('Only images from the rslethz organization are allowed');
+        }
 
         // check if docker socket is available
         if (!this.docker || !await this.docker.ping()) {
@@ -105,12 +113,26 @@ export class ContainerScheduler {
         logger.info('Creating container...');
         const container = await this.docker.createContainer({
             Image: container_options.docker_image,
-            name: 'datasets-runner-' + container_options.uuid,
+            name: ContainerScheduler.CONTAINER_PREFIX + container_options.uuid,
             Env: Object.entries(container_options.environment).map(([key, value]) => `${key}=${value}`),
             HostConfig: {
                 Memory: container_options.limits.memory_limit, // memory limit in bytes
                 NanoCpus: container_options.limits.cpu_limit, // CPU limit in nano CPUs
                 DiskQuota: container_options.limits.disk_quota,
+
+                // drop unnecessary default capabilities
+                CapDrop: [
+                    'CHOWN', 'DAC_OVERRIDE', 'FSETID', 'FOWNER', 'MKNOD',
+                    'NET_RAW', 'SETGID', 'SETUID', 'SETFCAP', 'SETPCAP', 'NET_BIND_SERVICE',
+                    'SYS_CHROOT', 'KILL', 'AUDIT_WRITE'
+                ],
+
+                SecurityOpt: ['no-new-privileges'],
+
+                // limits the number of processes the container can create
+                // this helps to prevent fork bombs / bugs in the container
+                // and helps to keep the base system stable even if the container is compromised
+                PidsLimit: 256,
             },
         }).catch((error) => {
 
@@ -133,26 +155,29 @@ export class ContainerScheduler {
         // TODO: check if there is a better solution which not used setTimeout
         setTimeout(async () => {
 
-            if (!container || !await container.inspect().catch(() => false)) {
+            if (!container || !await container.inspect().catch(dockerDaemonErrorHandling)) {
                 throw new Error(`Container ${container.id} not found, cannot stop it.`);
             }
 
-            if (!(await container.inspect()).State.Running) {
+            if (!(await container.inspect().catch(dockerDaemonErrorHandling))?.State.Running) {
                 logger.debug(`Container ${container.id} already stopped - no action required.`);
                 return;
             }
 
-            await container.stop();
+            await container.stop().catch(dockerDaemonErrorHandling);
             logger.info(`Runtime limit reached. Stopping container ${container.id}...`);
 
             // wait for max 10 seconds for the container to stop,
             // otherwise kill it
             setTimeout(async () => {
-                if ((await container.inspect()).State.Running) {
-                    await container.kill();
+                if ((await container.inspect().catch(dockerDaemonErrorHandling))?.State.Running) {
+                    await container.kill().catch(dockerDaemonErrorHandling)
                     logger.info(`Container ${container.id} killed after reaching runtime limit.`);
                 }
             }, 10_000);
+
+            // remove the container after stopping
+            await container.remove().catch(dockerDaemonErrorHandling);
 
         }, container_options.limits.max_runtime);
 
@@ -183,18 +208,115 @@ export class ContainerScheduler {
     }
 
     /**
+     * Checks all pending missions, if no container is running,
+     * it updates the state of the mission to 'FAILED'.
+     */
+    @tracing()
+    protected async cleanupContainers() {
+        logger.debug('Cleanup containers and dangling actions...');
+
+        const running_action_containers: Dockerode.ContainerInfo[] =
+            (await this.docker.listContainers({all: true})
+                .catch(dockerDaemonErrorHandling))
+                ?.filter((container: Dockerode.ContainerInfo) =>
+                    container.Names[0].startsWith(`/${ContainerScheduler.CONTAINER_PREFIX}`)
+                ) || [];
+
+        //////////////////////////////////////////////////////////////////////////////
+        // Find crashed containers
+        //////////////////////////////////////////////////////////////////////////////
+
+        const action_ids = running_action_containers
+            .map((container) => container.Names[0]
+                .replace(`/${ContainerScheduler.CONTAINER_PREFIX}_`, '')
+            );
+
+        const actions_in_process = await this.actionRepository.find({
+            where: {state: ActionState.PROCESSING},
+            relations: ['mission', 'mission.project'],
+        });
+        logger.info(`Checking ${actions_in_process.length} pending Actions.`);
+
+        for (const action of actions_in_process) {
+            if (!action_ids.includes(action.docker_image)) {
+                logger.info(`Action ${action.uuid} is running but has no running container.`);
+                action.state = ActionState.FAILED;
+                action.state_cause = 'Container crashed, no container found';
+                await this.actionRepository.save(action);
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////
+        // Kill Old Containers
+        //////////////////////////////////////////////////////////////////////////////
+
+        for (const container of running_action_containers) {
+
+            // try to find corresponding action
+            const action = await this.actionRepository.findOne({
+                where: {
+                    uuid: container.Names[0]
+                        .replace(`/${ContainerScheduler.CONTAINER_PREFIX}`, '')
+                },
+            });
+
+            // kill action container if no corresponding action is found
+            if (!action) {
+                logger.warn(`Container ${container.Id} has no corresponding action, killing it.`);
+                await this.docker.getContainer(container.Id).kill().catch(dockerDaemonErrorHandling)
+                await this.docker.getContainer(container.Id).remove().catch(dockerDaemonErrorHandling)
+                continue;
+            }
+
+            // ignore containers which are not in processing state
+            if (action?.state !== ActionState.PROCESSING) {
+
+                // kill if older than 24 hours
+                const created_at = new Date(container.Created * 1000);
+                const now = new Date();
+                const diff = now.getTime() - created_at.getTime();
+
+                if (diff > 1000 * 60 * 60 * 24) {
+                    logger.info(`Container for action ${action.uuid} is older than 24 hours, killing it.`);
+                    await this.docker.getContainer(container.Id).kill().catch(dockerDaemonErrorHandling)
+                    await this.docker.getContainer(container.Id).remove().catch(dockerDaemonErrorHandling)
+
+                    action.state = ActionState.FAILED;
+                    action.state_cause = 'Container killed: running for more than 24 hours';
+                    await this.actionRepository.save(action);
+
+                }
+
+            }
+
+            // kill and fail the action
+            logger.info(`Container for completed action ${action.uuid} found, killing it.`);
+            await this.docker.getContainer(container.Id).kill().catch(dockerDaemonErrorHandling)
+            await this.docker.getContainer(container.Id).remove().catch(dockerDaemonErrorHandling)
+
+            action.state = ActionState.FAILED;
+            action.state_cause = 'Container killed: action completed';
+            await this.actionRepository.save(action);
+
+        }
+
+    }
+
+    /**
      * Get logs from running or stopped container and return the logs as an array of objects.
      * Each object contains the timestamp, message, and type of the log (stdout or stderr).
      *
      * If the container is still running, the function will not return until the container stops.
      *
      * @param container - The container to get logs from
+     * @param action
      * @param sanitize_callback - A callback function to sanitize the log messages
      *
      */
     @tracing()
     protected async getContainerLogs(
-        container: Docker.Container,
+        container: Dockerode.Container,
+        action: Action,
         sanitize_callback?: (str: string) => string,
     ): Promise<ContainerLog[]> {
         const logs: ContainerLog[] = [];
@@ -202,7 +324,9 @@ export class ContainerScheduler {
         const container_logger = logger.child({
             labels: {
                 container_id: container.id,
-                job: ACTION_CONTAINER_LABEL,
+                action_uuid: action?.uuid || 'unknown',
+                mission_uuid: action?.mission?.uuid || 'unknown',
+                project_uuid: action?.mission?.project?.uuid || 'unknown',
             },
         });
 
@@ -225,7 +349,7 @@ export class ContainerScheduler {
                             message = sanitize_callback(message);
                         }
 
-                        logger.silly(message, { container_id: container.id });
+                        logger.silly(message, {container_id: container.id});
                         container_logger.info(message);
 
                         logs.push({
--- queueConsumer/src/actions/provider.ts
+++ queueConsumer/src/actions/provider.ts
@@ -1,13 +1,13 @@
 import logger from '../logger';
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { InjectQueue, OnQueueActive, OnQueueCompleted, OnQueueFailed, Process, Processor } from '@nestjs/bull';
-import { Job, Queue } from 'bull';
-import { tracing } from '../tracing';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { ActionState, KeyTypes } from '@common/enum';
+import {Injectable, OnModuleInit} from '@nestjs/common';
+import {InjectQueue, OnQueueActive, OnQueueCompleted, OnQueueFailed, Process, Processor} from '@nestjs/bull';
+import {Job, Queue} from 'bull';
+import {tracing} from '../tracing';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+import {ActionState, KeyTypes} from '@common/enum';
 import Action from '@common/entities/action/action.entity';
-import { ContainerEnv, ContainerScheduler } from './container_scheduler';
+import {ContainerEnv, ContainerScheduler} from './container_scheduler';
 import Apikey from '@common/entities/auth/apikey.entity';
 
 
@@ -18,11 +18,11 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
     constructor(
         @InjectQueue('action-queue') private readonly analysisQueue: Queue,
         @InjectRepository(Action)
-        private actionRepository: Repository<Action>,
+            actionRepository: Repository<Action>,
         @InjectRepository(Apikey)
         private apikeyRepository: Repository<Apikey>,
     ) {
-        super();
+        super(actionRepository);
         logger.debug('AnalysisProcessor constructor');
     }
 
@@ -34,12 +34,17 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
         } catch (error) {
             logger.error('Failed to connect to Redis:', error);
         }
+
+        // cleanup containers every 5 minutes
+        await this.cleanupContainers();
+        setInterval(async () => await this.cleanupContainers(), 1000 * 60 * 5);
+
     }
 
     // TODO: instead of concurrency we should use a more sophisticated way to limit the number of containers
     //  running at the same time by considering the resources available on the machine and the
     //  resources required by the containers (e.g., memory, CPU, disk space)
-    @Process({ concurrency: 1, name: 'actionProcessQueue' })
+    @Process({concurrency: 5, name: 'actionProcessQueue'})
     async process_action(job: Job<{ mission_action_id: string }>) {
         return await this.handleAction(job);
     }
@@ -55,7 +60,7 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
 
         // update the state of the action in the database
         const action = await this.actionRepository.findOneOrFail({
-            where: { uuid: job.data.mission_action_id },
+            where: {uuid: job.data.mission_action_id},
         });
         action.state = ActionState.DONE;
         await this.actionRepository.save(action);
@@ -69,110 +74,30 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
 
         // update the state of the action in the database
         const action = await this.actionRepository.findOneOrFail({
-            where: { uuid: job.data.mission_action_id },
+            where: {uuid: job.data.mission_action_id},
         });
         action.state = ActionState.FAILED;
         action.state_cause = error.message;
         await this.actionRepository.save(action);
     }
 
-    /**
-     * Checks all pending missions, if no container is running,
-     * it updates the state of the mission to 'FAILED'.
-     */
-    @tracing()
-    private async setCrashedContainersToFailed() {
-        logger.debug('Checking pending missions');
-
-        const actions = await this.actionRepository.find({
-            where: { state: ActionState.PROCESSING },
-            relations: ['mission', 'mission.project'],
-        });
-
-        logger.info(`Checking ${actions.length} pending Actions.`);
-
-        const container_ids = await this.docker.listContainers({ all: true });
-        const running_containers = container_ids.map(
-            (container) => container.Id,
-        );
-
-        for (const action of actions) {
-            if (!running_containers.includes(action.docker_image)) {
-                logger.info(
-                    `Action ${action.uuid} is running but has no running container. Setting state to 'FAILED'.`,
-                );
-                action.state = ActionState.FAILED;
-                action.state_cause = 'Container crashed';
-                await this.actionRepository.save(action);
-            }
-        }
-    }
-
-    /**
-     * Kills all containers (and tagged using this projects naming convention) that are older than i minutes.
-     * If it is 0, all containers are killed.
-     **
-     * @param killAge - The age of the containers
-     * @private
-     */
-    @tracing()
-    private async killOldContainers(killAge: number = 0) {
-        logger.info(`Killing containers older than ${killAge} minutes`);
-
-        const container_ids = await this.docker.listContainers({ all: true });
-
-        const now = new Date().getTime();
-        const killTime = now - killAge * 60 * 1000;
-
-        for (const container of container_ids) {
-            const containerInfo = await this.docker
-                .getContainer(container.Id)
-                .inspect();
-            const containerName = containerInfo.Name;
-            const containerCreated = new Date(containerInfo.Created).getTime();
-
-            if (
-                containerName.startsWith('datasets-runner-') &&
-                containerCreated < killTime
-            ) {
-                logger.info(
-                    `Killing container ${containerName} created at ${containerCreated}`,
-                );
-                await this.docker.getContainer(container.Id).kill();
-
-                // set state in the database
-                const uuid = containerName.split('-')[2];
-                const action = await this.actionRepository.findOne({
-                    where: { uuid: uuid },
-                    relations: ['mission', 'mission.project'],
-                });
-
-                action.state = ActionState.FAILED;
-                action.state_cause = 'Container killed.';
-                await this.actionRepository.save(action);
-            }
-        }
-    }
 
     @tracing('processing_action')
     private async handleAction(job: Job<{ mission_action_id: string }>) {
         logger.info(`\n\nProcessing Action ${job.data.mission_action_id}`);
 
-        // TODO: currently we allow only one container to mission at a time, we should change this to allow more
-        //  containers to mission concurrently
-        logger.info('Killing old containers and finding crashed containers');
-        await this.killOldContainers(0);
-        await this.setCrashedContainersToFailed();
-
         logger.info('Creating container.');
         const uuid = job.data.mission_action_id;
         const action = await this.actionRepository.findOne({
-            where: { uuid: uuid },
+            where: {uuid: uuid},
             relations: ['mission', 'mission.project'],
         });
 
         if (!action.uuid || action.uuid !== uuid)
             throw new Error('Action not found');
+
+        if (action.state !== ActionState.PENDING)
+            throw new Error(`Action state is not 'PENDING'`);
 
         // set state to 'RUNNING'
         action.state = ActionState.PROCESSING;
@@ -180,7 +105,7 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
 
         const now = new Date();
         const newToken = this.apikeyRepository.create({
-            mission: { uuid: action.mission.uuid },
+            mission: {uuid: action.mission.uuid},
             apikeytype: KeyTypes.CONTAINER,
             deletedAt: new Date(now.getTime() + 1000 * 60 * 60 * 24 * 7),
         });
@@ -196,7 +121,7 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
         const container = await this.start_container({
             docker_image: action.docker_image,
             uuid,
-            limits: { max_runtime: 10 * 1_000 }, // 10 seconds
+            limits: {max_runtime: 10 * 1_000}, // 60 seconds
             environment: env_variables,
         });
 
@@ -206,7 +131,7 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
 
         // get logs from container
         logger.info('Getting logs from container...');
-        action.logs = await this.getContainerLogs(container, sanitize);
+        action.logs = await this.getContainerLogs(container, action, sanitize);
         await this.actionRepository.save(action);
 
         // wait for the container to stop
@@ -221,6 +146,5 @@ export class ActionQueueProcessor extends ContainerScheduler implements OnModule
         return true; // mark the job as completed
 
     }
-
 
 }
