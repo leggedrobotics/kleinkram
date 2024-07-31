bcc82bf80dd31df5455b13d4ff6d847e5f8f0ec8
Cyrill Püntener <wp99cp@gmx.net>
2024-07-31 16:12:16 +0200

    use ref as queryKey instead of its value

--- frontend/src/hooks/customQueryHooks.ts
+++ frontend/src/hooks/customQueryHooks.ts
@@ -1,20 +1,20 @@
-import {ModelRef} from "vue";
+import {Ref} from "vue";
 import {useQuery, UseQueryReturnType} from "@tanstack/vue-query";
 import {Mission} from "src/types/Mission";
 import {getMission} from "src/services/queries/mission";
 import {Project} from "src/types/Project";
 import {getProject} from "src/services/queries/project";
 
-export const useMissionQuery = (mission_uuid: ModelRef<string | undefined>): UseQueryReturnType<Mission | null, Error> => {
+export const useMissionQuery = (mission_uuid: Ref<string | undefined>): UseQueryReturnType<Mission | null, Error> => {
     return useQuery<Mission | null>({
-        queryKey: ['mission', mission_uuid ? mission_uuid.value : ''],
+        queryKey: ['mission', mission_uuid ? mission_uuid : ''],
         queryFn: () => mission_uuid.value ? getMission(mission_uuid.value as string) : null,
         enabled: () => !!mission_uuid.value
     });
 }
-export const useProjectQuery = (project_uuid: ModelRef<string | undefined>): UseQueryReturnType<Project | null, Error> => {
+export const useProjectQuery = (project_uuid: Ref<string | undefined>): UseQueryReturnType<Project | null, Error> => {
     return useQuery<Project | null>({
-        queryKey: ['project', project_uuid ? project_uuid.value : ''],
+        queryKey: ['project', project_uuid ? project_uuid : ''],
         queryFn: () => project_uuid.value ? getProject(project_uuid.value as string) : null,
         enabled: () => !!project_uuid.value
     })
