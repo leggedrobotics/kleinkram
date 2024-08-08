3fc9864bf8e747af251fb1585adc5bb5d803d355
Cyrill Püntener <wp99cp@gmx.net>
2024-08-08 10:00:18 +0200

    implement: error page should not require login

--- frontend/src/boot/router.ts
+++ frontend/src/boot/router.ts
@@ -16,6 +16,11 @@ export default boot(({ router }) => {
             return;
         }
 
+        // redirect to error page is also fine
+        if (!Object.values(ROUTES).some((route) => route.path === to.path)) {
+            return;
+        }
+
         // check if the user is authenticated, if not redirect to login page
         const auth = await isAuthenticated();
         if (!auth && to.path !== ROUTES.LOGIN.path) {
