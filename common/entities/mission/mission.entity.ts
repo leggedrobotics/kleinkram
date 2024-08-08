440481a15e600686545a2ac0f6e8cc60e037eec4
Cyrill Püntener <wp99cp@gmx.net>
2024-08-08 09:45:56 +0200

    renaming token to api key (see #48)

--- common/entities/mission/mission.entity.ts
+++ common/entities/mission/mission.entity.ts
@@ -1,11 +1,10 @@
-import { Column, Entity, ManyToMany, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import BaseEntity from '../base-entity.entity';
 import Project from '../project/project.entity';
 import FileEntity from '../file/file.entity';
 import QueueEntity from '../queue/queue.entity';
 import User from '../user/user.entity';
 import Apikey from '../auth/apikey.entity';
-import AccessGroup from '../auth/accessgroup.entity';
 import Tag from '../tag/tag.entity';
 import MissionAccess from '../auth/mission_access.entity';
 
@@ -27,7 +26,7 @@ export default class Mission extends BaseEntity {
     creator: User;
 
     @OneToMany(() => Apikey, (token) => token.mission)
-    tokens: Apikey[];
+    api_keys: Apikey[];
 
     @OneToMany(() => MissionAccess, (mission_access) => mission_access.missions)
     mission_accesses: MissionAccess[];
