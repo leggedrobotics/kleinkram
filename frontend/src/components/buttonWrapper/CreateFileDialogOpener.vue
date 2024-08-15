c77557b0b88bf47cf59bb97c2fa110e76919cdf8
Johann Schwabe <johann@schwabe.dev>
2024-08-15 14:45:30 +0200

    fixed  #282

--- frontend/src/components/buttonWrapper/CreateFileDialogOpener.vue
+++ frontend/src/components/buttonWrapper/CreateFileDialogOpener.vue
@@ -7,12 +7,18 @@
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
 import CreateFileDialog from 'src/dialogs/CreateFileDialog.vue';
+import { Mission } from 'src/types/Mission';
 
 const $q = useQuasar();
-
+const props = defineProps<{
+    mission?: Mission;
+}>();
 const createNewTageType = () =>
     $q.dialog({
         title: 'Create new mission',
         component: CreateFileDialog,
+        componentProps: {
+            mission: props.mission,
+        },
     });
 </script>
