c358aa7ff6d4cba77e0d1e6f963ed0d15a7e1a28
Johann Schwabe <johann@schwabe.dev>
2024-08-15 18:51:21 +0200

    APIVersion in response header

---
+++ backend/src/versionInjector.ts
@@ -0,0 +1,24 @@
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import * as fs from 'node:fs';
+const packageJson = JSON.parse(
+    fs.readFileSync('/usr/src/app/backend/package.json', 'utf8'),
+);
+const appVersion = packageJson.version;
+
+@Injectable()
+export class AddVersionInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            tap(() => {
+                const res = context.switchToHttp().getResponse();
+                res.header('kleinkram-version', appVersion);
+            }),
+        );
+    }
+}
