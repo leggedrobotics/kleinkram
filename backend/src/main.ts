ccd21c0d02a77d357c197f9a019f9e4075ed7252
Johann Schwabe <johann@schwabe.dev>
2024-08-20 17:53:52 +0200

    fixed volumes for testing2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -103,8 +103,11 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalFilters(new GlobalErrorFilter());
     app.useGlobalPipes(new DelayPipe(0));
+    console.log('Listening on port 3000');
     await app.listen(3000);
+    console.log('Save endpoints as JSON');
     save_endpoints_as_json(app, '__generated__endpoints.json');
+    console.log('Endpoints saved');
 }
 
 bootstrap().catch((err) => {
