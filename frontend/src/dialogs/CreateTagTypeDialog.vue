3781762383cd596e4e1f8e8910f4300e9152a097
Cyrill Püntener <wp99cp@gmx.net>
2024-08-28 14:56:11 +0200

    style create tag type dialog

--- frontend/src/dialogs/CreateTagTypeDialog.vue
+++ frontend/src/dialogs/CreateTagTypeDialog.vue
@@ -3,7 +3,7 @@
         <template #title> Create Tag Type</template>
 
         <template #content>
-            <create-tag-type />
+            <create-tag-type ref="createTagType" />
         </template>
 
         <template #actions>
@@ -11,7 +11,12 @@
                 flat
                 label="Create Tag Type"
                 class="bg-button-primary"
-                @click="onDialogOK"
+                @click="
+                    () => {
+                        createTagType?.createTagTypeAction();
+                        onDialogOK();
+                    }
+                "
             />
         </template>
     </base-dialog>
@@ -20,6 +25,9 @@
 import { useDialogPluginComponent } from 'quasar';
 import CreateTagType from 'components/CreateTagType.vue';
 import BaseDialog from 'src/dialogs/BaseDialog.vue';
+import { ref } from 'vue';
+
+const createTagType = ref();
 
 const { dialogRef, onDialogOK } = useDialogPluginComponent();
 </script>
