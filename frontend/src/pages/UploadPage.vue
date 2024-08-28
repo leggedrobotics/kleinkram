b31316a3ced609d6449d041e229de7c937361b26
Cyrill Püntener <wp99cp@gmx.net>
2024-08-28 15:53:28 +0200

    remove create file from Pending Uploads page

--- frontend/src/pages/UploadPage.vue
+++ frontend/src/pages/UploadPage.vue
@@ -1,11 +1,9 @@
 <template>
     <title-section title="Pending Uploads"></title-section>
 
-    <create-file />
     <QueueItems />
 </template>
 <script setup lang="ts">
-import CreateFile from 'components/CreateFile.vue';
 import QueueItems from 'components/QueueItems.vue';
 import TitleSection from 'components/TitleSection.vue';
 </script>
