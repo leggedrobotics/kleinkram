cc6483014769916d5f071a7d092692436399a145
Cyrill Püntener <wp99cp@gmx.net>
2024-09-17 09:00:27 +0200

    fix #512

--- frontend/src/components/EditProject.vue
+++ frontend/src/components/EditProject.vue
@@ -1,38 +1,56 @@
 <template>
-    <q-input
-        ref="projectNameInput"
-        v-model="projectName"
-        outlined
-        autofocus
-        style="padding-bottom: 30px"
-        label="Project Name *"
-        :rules="[
-            (val) => !!val || 'A project name cannot be empty!',
-            (val) =>
-                !invalidProjectNames.includes(val) ||
-                'A project with that name already exists!',
-        ]"
-        @update:model-value="
-            () => {
-                hasValidInput =
-                    !!projectName &&
-                    !!projectDescription &&
-                    !invalidProjectNames.includes(projectName);
-            }
-        "
-    />
+    <div>
+        <label for="projectName">Project Name *</label>
+        <q-input
+            name="projectName"
+            ref="projectNameInput"
+            v-model="projectName"
+            outlined
+            autofocus
+            style="padding-bottom: 30px"
+            placeholder="Name..."
+            :rules="[
+                (val) => !!val || 'A project name cannot be empty!',
+                (val) =>
+                    val.length <= 20 ||
+                    'Project name must be less than 20 characters long!',
+                (val) =>
+                    val.length >= 3 ||
+                    'Project name must be at least 3 characters long!',
+                (val) =>
+                    /^[a-zA-Z0-9-_]*$/.test(val) ||
+                    `Project names can only contain letters, numbers, hyphens, and underscores! It contains: ${
+                        val
+                            .match(/[^a-zA-Z0-9-_]/g)
+                            .map((c: string) => `'${c}'`)
+                            .join(', ') || ''
+                    }`,
+                (val) =>
+                    !invalidProjectNames.includes(val) ||
+                    'A project with that name already exists!',
+            ]"
+            @update:model-value="
+                () => {
+                    hasValidInput = !!projectName && !!projectDescription;
+                }
+            "
+        />
 
-    <q-input
-        v-model="projectDescription"
-        type="textarea"
-        outlined
-        style="padding-bottom: 10px"
-        label="Project Description *"
-        :rules="[(val) => !!val || 'Project Description is required']"
-        @update:model-value="
-            hasValidInput = !!projectName && !!projectDescription
-        "
-    />
+        <label for="projectDescription">Project Description *</label>
+        <q-input
+            autofocus
+            name="projectDescription"
+            v-model="projectDescription"
+            type="textarea"
+            outlined
+            style="padding-bottom: 10px"
+            placeholder="Description..."
+            :rules="[(val) => !!val || 'Project Description is required']"
+            @update:model-value="
+                hasValidInput = !!projectName && !!projectDescription
+            "
+        />
+    </div>
 </template>
 <script setup lang="ts">
 import { useQuery, useQueryClient } from '@tanstack/vue-query';
@@ -45,7 +63,6 @@ import { updateProject } from 'src/services/mutations/project';
 const props = defineProps<{
     project_uuid: string;
 }>();
-const emit = defineEmits(['close']);
 const queryClient = useQueryClient();
 
 const projectName = ref('');
@@ -70,76 +87,74 @@ watch(
     },
     { immediate: true },
 );
-async function save() {
+
+async function save_changes(): Promise<void> {
+    // resolve immediately if no changes
     if (
         projectName.value === project.value?.name &&
         projectDescription.value === project.value?.description
-    ) {
-        return;
+    )
+        return Promise.resolve();
+
+    // validate input
+    if (!project.value?.uuid) {
+        return Promise.reject('Project is not valid');
     }
-    if (
-        project.value?.uuid &&
-        project.value?.name &&
-        project.value?.description
-    ) {
-        const noti = Notify.create({
-            group: false,
-            message: `Updating project ${project.value?.name}`,
-            color: 'info',
-            spinner: true,
-            timeout: 0,
-            position: 'bottom',
-        });
-        try {
-            await updateProject(
-                project.value?.uuid,
-                projectName.value.trim(),
-                projectDescription.value,
-            );
-            noti({
-                message: `Project ${project.value?.name} updated`,
-                color: 'positive',
-                spinner: false,
-                timeout: 3000,
+    if (!project.value?.name) {
+        return Promise.reject('Project name is not valid');
+    }
+    if (!project.value?.description) {
+        return Promise.reject('Project description is not valid');
+    }
+
+    await updateProject(
+        project.value?.uuid,
+        projectName.value.trim(),
+        projectDescription.value,
+    ).catch((error) => {
+        if (error.response.data.message.includes('Project')) {
+            Notify.create({
+                message:
+                    'Error updating project: ' + error.response.data.message,
+                color: 'negative',
+                position: 'bottom',
+                timeout: 5000,
             });
-            const cache = queryClient.getQueryCache();
-            const filtered = cache
-                .getAll()
-                .filter(
-                    (query) =>
-                        query.queryKey[0] === 'projects' ||
-                        query.queryKey[0] === 'project',
-                );
-            filtered.forEach((query) => {
-                queryClient.invalidateQueries({ queryKey: query.queryKey });
+        } else {
+            Notify.create({
+                message:
+                    'Error updating project: ' + error.response.data.message,
+                color: 'negative',
+                position: 'bottom',
+                timeout: 5000,
             });
-            emit('close');
-        } catch (e) {
-            if (
-                e.response.data.message ===
-                'Project with that name already exists'
-            ) {
-                invalidProjectNames.value.push(projectName.value);
-                noti({
-                    message: `Error updating project ${project.value?.name}: Project with that name already exists`,
-                    color: 'negative',
-                    spinner: false,
-                    timeout: 3000,
-                });
-            } else {
-                noti({
-                    message: `Error updating project ${project.value?.name}`,
-                    color: 'negative',
-                    spinner: false,
-                    timeout: 3000,
-                });
-            }
         }
-    }
+
+        return Promise.reject(error);
+    });
+
+    const cache = queryClient.getQueryCache();
+    const filtered = cache
+        .getAll()
+        .filter(
+            (query) =>
+                query.queryKey[0] === 'projects' ||
+                query.queryKey[0] === 'project',
+        );
+    filtered.forEach((query) => {
+        queryClient.invalidateQueries({ queryKey: query.queryKey });
+    });
+
+    Notify.create({
+        message: 'Project updated successfully',
+        color: 'positive',
+        position: 'bottom',
+        timeout: 2000,
+    });
 }
 
 defineExpose({
-    save,
+    save_changes,
 });
 </script>
 <style scoped></style>
