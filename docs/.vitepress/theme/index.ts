e23b71216a91f3b334c56b7c230f1ed4292f2dee
Cyrill Püntener <wp99cp@gmx.net>
2024-09-19 19:47:09 +0200

    fix documentation hydration error

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -9,7 +9,7 @@ import Read from '../components/AccessWrites/Read.vue';
 import Creator from '../components/AccessWrites/Creator.vue';
 
 export default {
-    ...DefaultTheme,
+    extends: DefaultTheme,
     enhanceApp({ app }) {
         // Register the Redoc component globally
         app.component('Module', Module);
