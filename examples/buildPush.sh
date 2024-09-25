bbd5d1ad533c122255776582b2432aba97391cf7
Johann Schwabe <johann@schwabe.dev>
2024-09-25 16:37:54 +0200

    one more

--- examples/buildPush.sh
+++ examples/buildPush.sh
@@ -1,6 +1,6 @@
 docker compose -f docker-compose.$1.yml build
 for service in $(docker compose -f docker-compose.$1.yml config --services); do
     image=$(docker compose -f docker-compose.$1.yml config | awk "/$service:$/ {flag=1} flag && /image:/ {print \$2; exit}")
-    echo "Pushing $image to Docker Hub...."
+    echo "Pushing $image to Docker Hub..."
     docker push "$image"
 done
