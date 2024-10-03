75eccdd1b6431ea63e5bea623558121c0d2d913d
Cyrill Püntener <wp99cp@gmx.net>
2024-10-03 15:46:18 +0200

    update big disk space action logs

--- examples/actions-disk-space/entrypoint.sh
+++ examples/actions-disk-space/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # allocate 20GB of disk space
-echo "Write 20GB of zeros to temp directory"
+echo "Write 20GB of zeros to /tmp_disk/disk_space_test"
 dd if=/dev/zero of=/tmp_disk/disk_space_test bs=1M count=20000
 
 echo "Wait for 30s"
