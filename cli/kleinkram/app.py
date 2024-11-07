3da25496d1651495158a2a99631eded4deef81b2
Dominique Garmier <dominique@garmier.ch>
2024-11-07 09:37:04 +0100

    removed dead code

--- cli/kleinkram/app.py
+++ cli/kleinkram/app.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional
+from typing import Optional, List
 
 import typer
 
@@ -8,12 +8,17 @@ from kleinkram.api.client import AuthenticatedClient
 from kleinkram.api.routes import claim_admin
 from kleinkram.auth import login_flow
 from kleinkram.commands.download import download_typer
+from kleinkram.commands.upload import upload_typer
+from kleinkram.commands.verify import verify_typer
 from kleinkram.commands.endpoint import endpoint_typer
 from kleinkram.commands.list import list_typer
-from kleinkram.commands.upload import upload_typer
+from kleinkram.commands.mission import mission_typer
+from kleinkram.commands.project import project_typer
 from kleinkram.config import Config
 from kleinkram.config import get_shared_state
 from kleinkram.utils import get_version
+from typer.core import TyperGroup
+from click import Context
 
 
 CLI_HELP = """\
@@ -25,15 +30,28 @@ https://docs.datasets.leggedrobotics.com/usage/cli/cli-getting-started.html \
 for more information.
 """
 
+
+class OrderCommands(TyperGroup):
+    def list_commands(self, ctx: Context) -> List[str]:
+        _ = ctx  # suppress unused variable warning
+
+        # TODO: sort commands
+        return list(self.commands)
+
+
 app = typer.Typer(
+    cls=OrderCommands,
     help=CLI_HELP,
     context_settings={"help_option_names": ["-h", "--help"]},
 )
 
-app.add_typer(endpoint_typer, name="endpoint")
 app.add_typer(download_typer, name="download")
 app.add_typer(upload_typer, name="upload")
+app.add_typer(verify_typer, name="verify")
 app.add_typer(list_typer, name="list")
+app.add_typer(endpoint_typer, name="endpoint")
+app.add_typer(mission_typer, name="mission")
+app.add_typer(project_typer, name="project")
 
 
 @app.command()
@@ -54,7 +72,7 @@ def logout(all: bool = typer.Option(False, help="logout on all enpoints")) -> No
 def claim():
     client = AuthenticatedClient()
     claim_admin(client)
-    print("Admin rights claimed successfully.")
+    print("admin rights claimed successfully.")
 
 
 def _version_cb(value: bool) -> None:
