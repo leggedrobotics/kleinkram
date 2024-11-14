b9ee7cb96115dfb60a61ecf99f3d25a234264453
lars <lleuthold@ethz.ch>
2024-11-14 09:54:44 +0100

    unnecessary comments and imports deleted

--- frontend/src/components/buttonWrapper/DeleteActionDialogOpener.vue
+++ frontend/src/components/buttonWrapper/DeleteActionDialogOpener.vue
@@ -21,39 +21,35 @@
 </style>
 
 <script setup lang="ts">
-import { Notify, useQuasar } from 'quasar';
-import EditProjectDialog from 'src/dialogs/EditProjectDialog.vue';
+import { useQuasar } from 'quasar';
 import {
-    canModifyProject,
     getPermissionForMission,
     getPermissionForProject,
     usePermissionsQuery,
 } from 'src/hooks/customQueryHooks';
-import { computed, ComputedRef, ref, watch, watchEffect } from 'vue';
-import { useMutation, useQueryClient } from '@tanstack/vue-query';
-import { deleteAction } from 'src/services/mutations/action';
+import { computed, ref, watchEffect } from 'vue';
+import { useQueryClient } from '@tanstack/vue-query';
 import DeleteActionDialog from 'src/dialogs/DeleteActionDialog.vue';
 import { Action } from 'src/types/Action';
 import { getMe } from 'src/services/queries/user';
 
 const $q = useQuasar();
-//not passed correctly yet in Action
+
 const props = defineProps({
     action: Action,
 });
 
 const { data: permissions } = usePermissionsQuery();
-// const me = await getMe();
+
 const canDelete = computed(
     () => deletePermissions.value >= 30 || creatorDeleteRights.value,
-    // canModifyProject(project_uuid, permissions.value),
 );
 
 const creatorDeleteRights = ref<boolean>(false);
 
 watchEffect(async () => {
     const me = await getMe();
-    // const creatorDeleteRights = computed(() => {
+
     const actionCreator = props.action?.createdBy;
     creatorDeleteRights.value = me.uuid === actionCreator?.uuid;
 });
@@ -86,36 +82,4 @@ const openDeleteActionDialog = () => {
 };
 
 const queryClient = useQueryClient();
-
-//this will prob not be needed
-// const { mutateAsync: deleteActionMutation } = useMutation({
-//     mutationFn: (action: Action) => {
-//         return deleteAction(action);
-//     },
-//     onSuccess: () => {
-//         queryClient.invalidateQueries({
-//             predicate: (query) => {
-//                 //
-//                 return query.queryKey[0] === 'action_mission';
-//             },
-//         });
-//         Notify.create({
-//             group: false,
-//             message: 'Template created',
-//             color: 'positive',
-//             position: 'bottom',
-//             timeout: 2000,
-//         });
-//     },
-//     onError: (error) => {
-//         console.error(error);
-//         Notify.create({
-//             group: false,
-//             message: `Error: ${error?.response?.data.message}`,
-//             color: 'negative',
-//             position: 'bottom',
-//             timeout: 2000,
-//         });
-//     },
-// });
 </script>
