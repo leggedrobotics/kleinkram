1ad406939fcb657a84cab6ae4c48ec760140bf05
Dominique Garmier <dominique@garmier.ch>
2024-12-16 11:07:30 +0100

    tests: added tests for some of the helper functions in core

--- cli/kleinkram/__init__.py
+++ cli/kleinkram/__init__.py
@@ -1,27 +1,35 @@
 from __future__ import annotations
 
 from kleinkram._version import __version__
+from kleinkram.core import create_mission
+from kleinkram.core import create_project
+from kleinkram.core import delete_file
+from kleinkram.core import delete_mission
+from kleinkram.core import delete_project
 from kleinkram.core import download
 from kleinkram.core import list_files
 from kleinkram.core import list_missions
 from kleinkram.core import list_projects
+from kleinkram.core import update_file
 from kleinkram.core import update_mission
+from kleinkram.core import update_project
 from kleinkram.core import upload
 from kleinkram.core import verify
-from kleinkram.resources import FileSpec
-from kleinkram.resources import MissionSpec
-from kleinkram.resources import ProjectSpec
 
 __all__ = [
     "__version__",
-    "FileSpec",
-    "MissionSpec",
-    "ProjectSpec",
     "upload",
     "verify",
     "download",
     "list_files",
     "list_missions",
     "list_projects",
+    "update_file",
     "update_mission",
+    "update_project",
+    "delete_file",
+    "delete_mission",
+    "delete_project",
+    "create_mission",
+    "create_project",
 ]
