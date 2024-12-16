376a22ca625be41530027659ed5744f792e04b28
Dominique Garmier <dominique@garmier.ch>
2024-12-16 09:51:03 +0100

    chore: deleted a file

--- cli/t.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from kleinkram import download
-from kleinkram.config import Config
-from kleinkram.config import endpoint_table
-
-c = Config()
-tb = endpoint_table(c)
