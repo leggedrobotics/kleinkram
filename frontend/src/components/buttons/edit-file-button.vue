fb70f6acd8a8a51b21ad7be8bc35c49396cbbb90
Cyrill Püntener <wp99cp@gmx.net>
2024-12-17 11:42:29 +0100

    fix disabled file edit button

--- frontend/src/components/buttons/edit-file-button.vue
+++ frontend/src/components/buttons/edit-file-button.vue
@@ -5,10 +5,13 @@
         color="primary"
         icon="sym_o_edit"
         label="Edit File"
-        :disable="isEnabled"
+        :disable="!isEnabled"
         @click="editFile"
     >
-        <q-tooltip> Edit File</q-tooltip>
+        <q-tooltip v-if="isEnabled"> Edit File</q-tooltip>
+        <q-tooltip v-else>
+            You do not have permission to edit this file
+        </q-tooltip>
     </q-btn>
 </template>
 
