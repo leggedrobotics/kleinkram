9ef01eceda74772f02b4b1704ab0c0ec40b11ef9
Dominique Garmier <dominique@garmier.ch>
2025-01-14 09:01:03 +0100

    chore: moved custom validator to proper loation

--- common/api/types/mission/mission-query.dto.ts
+++ common/api/types/mission/mission-query.dto.ts
@@ -1,4 +1,5 @@
 import { ProjectQueryDto } from '../project/project-query.dto';
+import { IsRecordStringString } from '../../../validation/record-validation';
 import {
     IsArray,
     IsNotEmptyObject,
@@ -8,35 +9,6 @@ import {
     IsUUID,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { ValidateBy, ValidationOptions, buildMessage } from 'class-validator';
-
-function isRecordStringString(value: unknown): boolean {
-    if (typeof value !== 'object' || value === null) {
-        return false;
-    }
-    return Object.entries(value).every(
-        ([key, val]) => typeof key === 'string' && typeof val === 'string',
-    );
-}
-
-export function IsRecordStringString(
-    validationOptions?: ValidationOptions,
-): PropertyDecorator {
-    return ValidateBy({
-        name: 'IsRecordStringString',
-        constraints: [],
-        validator: {
-            validate: (value: unknown, _): boolean =>
-                isRecordStringString(value),
-            defaultMessage: buildMessage(
-                (eachPrefix) =>
-                    eachPrefix +
-                    '$property must be a record of string key-value pairs',
-                validationOptions,
-            ),
-        },
-    });
-}
 
 export class MissionQueryDto extends ProjectQueryDto {
     @IsOptional()
