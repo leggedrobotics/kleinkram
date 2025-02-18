066e12ec2a98244ebb29859d182fc65617c9305f
Cyrill Püntener <wp99cp@gmx.net>
2025-02-18 11:18:06 +0100

    fix "cannot view action details"

--- common/api/types/actions/docker-image.dto.ts
+++ common/api/types/actions/docker-image.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsArray, IsOptional, IsString } from 'class-validator';
 
 export class DockerImageDto {
     @ApiProperty()
-    @ValidateNested()
-    @Type(() => String)
+    @IsArray()
+    @IsString({ each: true })
     repoDigests!: string[];
+
+    @ApiProperty({ description: 'SHA value', required: false })
+    @IsOptional()
+    @IsString()
+    sha!: string;
 }
