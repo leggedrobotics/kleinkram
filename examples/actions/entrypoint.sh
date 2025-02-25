881fd2cd88cfdb2e7bf2342552425738e8e7a32f
Cyrill Püntener <wp99cp@gmx.net>
2025-02-25 16:48:10 +0100

    actions: use better env variable namings #1253

--- examples/actions/entrypoint.sh
+++ examples/actions/entrypoint.sh
@@ -5,23 +5,23 @@ set -e # exit on error
 env
 echo ""
 echo ""
-echo "List files of mission with UUID $MISSION_UUID"
+echo "List files of mission with UUID $KLEINKRAM_MISSION_UUID"
 echo ""
 
 # print rocket emoji
 echo "🚀 Rocket"
 
-klein endpoint set $ENDPOINT
-klein login --key $APIKEY
+klein endpoint server $KLEINKRAM_API_ENDPOINT $KLEINKRAM_S3_ENDPOINT
+klein login --key $KLEINKRAM_API_KEY
 
 klein --version
 
 # TODO... this Endpoint needs to be fixed
-klein list files -m $MISSION_UUID
-klein download -m $MISSION_UUID --dest /out
+klein list files -m $KLEINKRAM_MISSION_UUID
+klein download -m $KLEINKRAM_MISSION_UUID --dest /out
 
 echo ""
-echo "List files of mission with UUID $MISSION_UUID"
+echo "List files of mission with UUID $KLEINKRAM_MISSION_UUID"
 cd ./out || exit 1
 ls -la
 
