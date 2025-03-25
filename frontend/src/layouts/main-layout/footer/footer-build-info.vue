fadcc3eaacd81425c6bd7c92e933d898c8a00855
Cyrill Püntener <wp99cp@gmx.net>
2025-03-25 13:07:18 +0100

    make prettier happy

--- frontend/src/layouts/main-layout/footer/footer-build-info.vue
+++ frontend/src/layouts/main-layout/footer/footer-build-info.vue
@@ -18,7 +18,8 @@
         "
         >{{ BUILD_INFO.version }} ({{
             new Date(BUILD_INFO.timestamp).toLocaleString()
-        }})</a>
+        }})</a
+    >
     <footer-separator />
     <a
         class="text-placeholder"
