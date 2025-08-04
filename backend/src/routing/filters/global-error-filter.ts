96e44cc694f9cf7eb2e8b319dbba66a825ea8977
Cyrill Püntener <wp99cp@gmx.net>
2025-08-04 15:52:18 +0200

    fix unnecessary type assertion

--- backend/src/routing/filters/global-error-filter.ts
+++ backend/src/routing/filters/global-error-filter.ts
@@ -51,8 +51,7 @@ export class GlobalErrorFilter implements ExceptionFilter {
 
         if (exception instanceof AuthFlowException) {
             // If the exception is an AuthFlowException, redirect to the frontend login page
-            const authFlowException: AuthFlowException =
-                exception as AuthFlowException;
+            const authFlowException: AuthFlowException = exception;
 
             logger.debug(
                 `Redirecting to login with error: ${authFlowException.message}`,
