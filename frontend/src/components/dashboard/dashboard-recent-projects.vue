8e4f18a417ab0734ea345e060ae69cb5e5708cc8
Cyrill Püntener <wp99cp@gmx.net>
2025-11-11 17:03:32 +0100

    use correct icon for empty project state

--- frontend/src/components/dashboard/dashboard-recent-projects.vue
+++ frontend/src/components/dashboard/dashboard-recent-projects.vue
@@ -73,7 +73,7 @@
         <!-- Empty State -->
         <div v-else class="empty-state-wrapper">
             <div class="empty-state-content">
-                <q-icon name="sym_o_folder" size="lg" color="grey-6" />
+                <q-icon name="sym_o_box" size="lg" color="grey-6" />
                 <span class="text-h6 text-grey-7 q-mt-md">
                     No recent projects
                 </span>
